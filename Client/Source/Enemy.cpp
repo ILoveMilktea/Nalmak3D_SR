47e9720fef3e34356bc2538b4cb9a8695ab7be7c
JeonJohnson <weiding11086@gmail.com>
2021-02-15 20:40:46 +0900

    전근희 에너미 매니저
    1. EnemyManager 생성
    2. Enemy 클래스 생성
    3. HeeTestScene 생성
    210215

---
+++ Client/Source/Enemy.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "..\Include\Enemy.h"
+
+
+
+Enemy::Enemy(Desc * _Desc)
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::Initialize()
+{
+}
+
+void Enemy::Update()
+{
+}
---
+++ Client/Source/HeeTestScene.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "..\Include\HeeTestScene.h"
+
+
+HeeTestScene::HeeTestScene()
+{
+}
+
+
+HeeTestScene::~HeeTestScene()
+{
+}
+
+void HeeTestScene::Initialize()
+{
+}
