2bd0ff51d4b04cd487242f7ac3c62abc05a9f6f6
JUSIN-20210118P\Administrator <cjy5595@naver.com>
2021-03-04 17:50:29 +0900

    filters_Test

---
+++ Client/Include/FieldCameraZoomInState.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "C:\Users\Administrator.JUSIN-20210118P\Desktop\Nalmak3D_SR\Ref\Include\IState.h"
+class FieldCameraZoomInState :
+	public IState
+{
+public:
+	FieldCameraZoomInState();
+	virtual ~FieldCameraZoomInState();
+};
+
---
+++ Client/Source/FieldCameraZoomInState.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "..\Include\FieldCameraZoomInState.h"
+
+
+FieldCameraZoomInState::FieldCameraZoomInState()
+{
+}
+
+
+FieldCameraZoomInState::~FieldCameraZoomInState()
+{
+}
